7e828a47548af01036128134ea803f84dbc84cce
aarn-mcs <aarn@mexcorpsoluciones.com>
2015-12-19 16:51:42 -0600

    Dashboard view

--- resources/views/auth/index.blade.php
+++ resources/views/auth/index.blade.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html class="no-js css-menubar" lang="en">
 <head>
@@ -43,66 +42,69 @@
     <style>
         @media (max-width: 4000px) and (min-width: 0px) {
             .page-login-v2 .page-login-main {
-              padding-top: 0px !important;
+                padding-top: 0px !important;
             }
-        }g
+        }
+
+        g
     </style>
 </head>
 <body class="page-login-v2 layout-full page-dark login">
 <!--[if lt IE 8]>
-<p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+<p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade
+    your browser</a> to improve your experience.</p>
 <![endif]-->
 <!-- Page -->
 <div class="page animsition" data-animsition-in="fade-in" data-animsition-out="fade-out">
     <div class="page-content">
         <div class="page-brand-info">
             <div class="brand">
-                <img class="brand-img" src="/assets/images/logo@2x.png" alt="...">
-                <h2 class="brand-text font-size-40">Remark</h2>
+                <img class="brand-img" src="/assets/images/logos/dream_house.png" alt="Dream House">
+                {{--<h2 class="brand-text font-size-40">Remark</h2>--}}
             </div>
             <p class="font-size-20">Texto que se ve en resoluciones grandes</p>
         </div>
         <div class="page-login-main" style="padding-top: 80px !important; padding-bottom : 0px !important;">
             <div class="brand visible-xs">
-                <img class="brand-img" src="/assets/images/logo-blue@2x.png" alt="...">
-                <h3 class="brand-text font-size-40">Remark</h3>
+                <img class="brand-img" src="/assets/images/logos/dream_house.png" alt="Dream House" style="align-items: center;">
+                {{--<h3 class="brand-text font-size-40">Remark</h3>--}}
             </div>
             <h3 class="font-size-24">Sign In</h3>
             <p>Texto que se ve en reoluciones pequeñas</p>
             {!! Form::open(['route'=>'auth.login','method'=>'post']) !!}
-                <div class="form-group">
-                    <label class="sr-only" for="inputEmail">User</label>
-                    <input type="text" class="form-control" id="inputEmail" name="user" placeholder="User">
+            <div class="form-group">
+                <label class="sr-only" for="inputEmail">User</label>
+                <input type="text" class="form-control" id="inputEmail" name="user" placeholder="User">
+            </div>
+            <div class="form-group">
+                <label class="sr-only" for="inputPassword">Password</label>
+                <input type="password" class="form-control" id="inputPassword" name="password"
+                       placeholder="Password">
+            </div>
+            <div class="form-group clearfix">
+                <div class="checkbox-custom checkbox-inline checkbox-primary pull-left">
+                    <input type="checkbox" id="remember" name="checkbox">
+                    <label for="inputCheckbox">Remember me</label>
                 </div>
-                <div class="form-group">
-                    <label class="sr-only" for="inputPassword">Password</label>
-                    <input type="password" class="form-control" id="inputPassword" name="password"
-                           placeholder="Password">
-                </div>
-                <div class="form-group clearfix">
-                    <div class="checkbox-custom checkbox-inline checkbox-primary pull-left">
-                        <input type="checkbox" id="remember" name="checkbox">
-                        <label for="inputCheckbox">Remember me</label>
-                    </div>
-                    <a class="pull-right" href="forgot-password.html">Forgot password?</a>
-                </div>
-                <button type="submit" class="btn btn-primary btn-block">Sign in</button>
+                <a class="pull-right" href="forgot-password.html">Forgot password?</a>
+            </div>
+            <button type="submit" class="btn btn-primary btn-block">Sign in</button>
             {!! Form::close() !!}
             <p>No account? <a href="register-v2.html">Sign Up</a></p>
             {{--<footer class="page-copyright">--}}
-                {{--<p>WEBSITE BY amazingSurge</p>--}}
-                {{--<p>© 2015. All RIGHT RESERVED.</p>--}}
-                {{--<div class="social">--}}
-                    {{--<a class="btn btn-icon btn-round social-twitter" href="javascript:void(0)">--}}
-                        {{--<i class="icon bd-twitter" aria-hidden="true"></i>--}}
-                    {{--</a>--}}
-                    {{--<a class="btn btn-icon btn-round social-facebook" href="javascript:void(0)">--}}
-                        {{--<i class="icon bd-facebook" aria-hidden="true"></i>--}}
-                    {{--</a>--}}
-                    {{--<a class="btn btn-icon btn-round social-google-plus" href="javascript:void(0)">--}}
-                        {{--<i class="icon bd-google-plus" aria-hidden="true"></i>--}}
-                    {{--</a>--}}
-                {{--</div>--}}
+            {{--<p>WEBSITE BY amazingSurge</p>--}}
+            {{--<p>© 2015. All RIGHT RESERVED.</p>--}}
+            {{--<div class="social">--}}
+            {{--<a class="btn btn-icon btn-round social-twitter" href="javascript:void(0)">--}}
+            {{--<i class="icon bd-twitter" aria-hidden="true"></i>--}}
+            {{--</a>--}}
+            {{--<a class="btn btn-icon btn-round social-facebook" href="javascript:void(0)">--}}
+            {{--<i class="icon bd-facebook" aria-hidden="true"></i>--}}
+            {{--</a>--}}
+            {{--<a class="btn btn-icon btn-round social-google-plus" href="javascript:void(0)">--}}
+            {{--<i class="icon bd-google-plus" aria-hidden="true"></i>--}}
+            {{--</a>--}}
+            {{--</div>--}}
             {{--</footer>--}}
         </div>
     </div>
@@ -138,10 +140,10 @@
 <script src="/js/components/tabs.js"></script>
 <script src="/js/components/jquery-placeholder.js"></script>
 <script>
-    (function(document, window, $) {
+    (function (document, window, $) {
         'use strict';
         var Site = window.Site;
-        $(document).ready(function() {
+        $(document).ready(function () {
             Site.run();
         });
     })(document, window, jQuery);
