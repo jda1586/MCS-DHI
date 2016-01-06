71a933f5e89bb8b7c9fd38d8c5b81ace49f0e1d0
aarn-mcs <aarn@mexcorpsoluciones.com>
2016-01-06 01:39:59 -0600

    dashboard v1.0

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -32,147 +32,24 @@
 @section('content')
     <div class="content">
         <div class="row" style="padding: 25px 0;">
-            {{--<div class="col-md-12">--}}
-            {{--<div class="col-md-12 panel" style="--}}
-            {{--margin-bottom: 15px;--}}
-            {{--height: 220px;">--}}
-            {{--<!-- Example Multiple Items -->--}}
-            {{--<div class="example-wrap">--}}
-            {{--<h4 class="example-title">Multiple Items</h4>--}}
-            {{--<div class="example">--}}
-            {{--<div class="slider slick-initialized slick-slider" id="exampleMultipleItems">--}}
-            {{--<button type="button" data-role="none" class="slick-prev slick-arrow"--}}
-            {{--aria-label="Previous"--}}
-            {{--role="button" style="display: block;">Previous--}}
-            {{--</button>--}}
-            {{--<div aria-live="polite" class="slick-list draggable">--}}
-            {{--<div class="slick-track" role="listbox"--}}
-            {{--style="opacity: 1; width: 2115px; transform: translate3d(-846px, 0px, 0px);">--}}
-            {{--<div class="slick-slide slick-cloned" data-slick-index="-3" aria-hidden="true"--}}
-            {{--tabindex="-1" style="width: 141px;">--}}
-            {{--<h3>7</h3>--}}
-            {{--</div>--}}
-            {{--<div class="slick-slide slick-cloned" data-slick-index="-2" aria-hidden="true"--}}
-            {{--tabindex="-1" style="width: 141px;">--}}
-            {{--<h3>8</h3>--}}
-            {{--</div>--}}
-            {{--<div class="slick-slide slick-cloned" data-slick-index="-1" aria-hidden="true"--}}
-            {{--tabindex="-1" style="width: 141px;">--}}
-            {{--<h3>9</h3>--}}
-            {{--</div>--}}
-            {{--<div class="slick-slide" data-slick-index="0" aria-hidden="true" tabindex="-1"--}}
-            {{--role="option" aria-describedby="slick-slide10" style="width: 141px;">--}}
-            {{--<h3>1</h3>--}}
-            {{--</div>--}}
-            {{--<div class="slick-slide" data-slick-index="1" aria-hidden="true" tabindex="-1"--}}
-            {{--role="option" aria-describedby="slick-slide11" style="width: 141px;">--}}
-            {{--<h3>2</h3>--}}
-            {{--</div>--}}
-            {{--<div class="slick-slide" data-slick-index="2" aria-hidden="true" tabindex="-1"--}}
-            {{--role="option" aria-describedby="slick-slide12" style="width: 141px;">--}}
-            {{--<h3>3</h3>--}}
-            {{--</div>--}}
-            {{--<div class="slick-slide slick-current slick-active" data-slick-index="3"--}}
-            {{--aria-hidden="false" tabindex="0" role="option"--}}
-            {{--aria-describedby="slick-slide13"--}}
-            {{--style="width: 141px;">--}}
-            {{--<h3>4</h3>--}}
-            {{--</div>--}}
-            {{--<div class="slick-slide slick-active" data-slick-index="4" aria-hidden="false"--}}
-            {{--tabindex="0" role="option" aria-describedby="slick-slide14"--}}
-            {{--style="width: 141px;">--}}
-            {{--<h3>5</h3>--}}
-            {{--</div>--}}
-            {{--<div class="slick-slide slick-active" data-slick-index="5" aria-hidden="false"--}}
-            {{--tabindex="0" role="option" aria-describedby="slick-slide15"--}}
-            {{--style="width: 141px;">--}}
-            {{--<h3>6</h3>--}}
-            {{--</div>--}}
-            {{--<div class="slick-slide" data-slick-index="6" aria-hidden="true" tabindex="-1"--}}
-            {{--role="option" aria-describedby="slick-slide16" style="width: 141px;">--}}
-            {{--<h3>7</h3>--}}
-            {{--</div>--}}
-            {{--<div class="slick-slide" data-slick-index="7" aria-hidden="true" tabindex="-1"--}}
-            {{--role="option" aria-describedby="slick-slide17" style="width: 141px;">--}}
-            {{--<h3>8</h3>--}}
-            {{--</div>--}}
-            {{--<div class="slick-slide" data-slick-index="8" aria-hidden="true" tabindex="-1"--}}
-            {{--role="option" aria-describedby="slick-slide18" style="width: 141px;">--}}
-            {{--<h3>9</h3>--}}
-            {{--</div>--}}
-            {{--<div class="slick-slide slick-cloned" data-slick-index="9" aria-hidden="true"--}}
-            {{--tabindex="-1" style="width: 141px;">--}}
-            {{--<h3>1</h3>--}}
-            {{--</div>--}}
-            {{--<div class="slick-slide slick-cloned" data-slick-index="10" aria-hidden="true"--}}
-            {{--tabindex="-1" style="width: 141px;">--}}
-            {{--<h3>2</h3>--}}
-            {{--</div>--}}
-            {{--<div class="slick-slide slick-cloned" data-slick-index="11" aria-hidden="true"--}}
-            {{--tabindex="-1" style="width: 141px;">--}}
-            {{--<h3>3</h3>--}}
-            {{--</div>--}}
-            {{--</div>--}}
-            {{--</div>--}}
-
-
-            {{--<button type="button" data-role="none" class="slick-next slick-arrow" aria-label="Next"--}}
-            {{--role="button" style="display: block;">Next--}}
-            {{--</button>--}}
-            {{--</div>--}}
-            {{--</div>--}}
-            {{--</div>--}}
-            {{--<!-- End Example Multiple Items -->--}}
-            {{--</div>--}}
-            {{--</div>--}}
             <div class="col-md-3">
 
                 <div class="col-lg-12 col-sm-12 panel">
-                    <!-- Example Tags As Badges -->
                     <div class="example-wrap">
                         <h4 class="example-title">Categorias</h4>
                         <div class="example">
                             <div data-plugin="treeview" data-source="getExampleTreeview"
                                  data-expand-icon="icon md-check" data-collapse-icon="icon md-check-circle"
                                  data-node-icon="icon md-account" data-show-tags="true" id="exampleTagsAsBadges">
-                                {{--<ul class="list-group">
-                                    <li class="list-group-item node-exampleTagsAsBadges" data-nodeid="0"
-                                        style="color:undefined;background-color:undefined;"><span
-                                                class="icon expand-icon md-check"></span><span
-                                                class="icon node-icon md-account"></span>Parent 1<span
-                                                class="badge">4</span></li>
-                                    <li class="list-group-item node-exampleTagsAsBadges node-selected"
-                                        data-nodeid="5" style="color:#ffffff;background-color:#3949ab;"><span
-                                                class="icon"></span><span
-                                                class="icon node-icon md-account"></span>Parent 2<span
-                                                class="badge">0</span></li>
-                                    <li class="list-group-item node-exampleTagsAsBadges" data-nodeid="6"
-                                        style="color:undefined;background-color:undefined;"><span
-                                                class="icon"></span><span
-                                                class="icon node-icon md-account"></span>Parent 3<span
-                                                class="badge">0</span></li>
-                                    <li class="list-group-item node-exampleTagsAsBadges" data-nodeid="7"
-                                        style="color:undefined;background-color:undefined;"><span
-                                                class="icon"></span><span
-                                                class="icon node-icon md-account"></span>Parent 4<span
-                                                class="badge">0</span></li>
-                                    <li class="list-group-item node-exampleTagsAsBadges" data-nodeid="8"
-                                        style="color:undefined;background-color:undefined;"><span
-                                                class="icon"></span><span
-                                                class="icon node-icon md-account"></span>Parent 5<span
-                                                class="badge">0</span></li>
-                                </ul>--}}
                             </div>
                         </div>
                     </div>
-                    <!-- End Example Tags As Badges -->
                 </div>
 
             </div>
 
             <div class="col-md-9">
                 <div class="col-md-4 col-xs-12 masonry-item">
-                    <!-- Widget -->
                     <div class="widget widget-article widget-shadow">
                         <div class="widget-header cover overlay overlay-hover">
                             <img class="cover-image overlay-scale" src="/assets/images/logos/dream_house.png" alt="">
@@ -202,10 +79,8 @@
                             </div>
                         </div>
                     </div>
-                    <!-- End Widget -->
                 </div>
                 <div class="col-md-4 col-xs-12 masonry-item">
-                    <!-- Widget -->
                     <div class="widget widget-article widget-shadow">
                         <div class="widget-header cover overlay overlay-hover">
                             <img class="cover-image overlay-scale" src="/assets/images/logos/dream_house.png" alt="">
@@ -235,10 +110,8 @@
                             </div>
                         </div>
                     </div>
-                    <!-- End Widget -->
                 </div>
                 <div class="col-md-4 col-xs-12 masonry-item">
-                    <!-- Widget -->
                     <div class="widget widget-article widget-shadow">
                         <div class="widget-header cover overlay overlay-hover">
                             <img class="cover-image overlay-scale" src="/assets/images/logos/dream_house.png" alt="">
@@ -268,10 +141,8 @@
                             </div>
                         </div>
                     </div>
-                    <!-- End Widget -->
                 </div>
                 <div class="col-md-4 col-xs-12 masonry-item">
-                    <!-- Widget -->
                     <div class="widget widget-article widget-shadow">
                         <div class="widget-header cover overlay overlay-hover">
                             <img class="cover-image overlay-scale" src="/assets/images/logos/dream_house.png" alt="">
@@ -301,10 +172,8 @@
                             </div>
                         </div>
                     </div>
-                    <!-- End Widget -->
                 </div>
                 <div class="col-md-4 col-xs-12 masonry-item">
-                    <!-- Widget -->
                     <div class="widget widget-article widget-shadow">
                         <div class="widget-header cover overlay overlay-hover">
                             <img class="cover-image overlay-scale" src="/assets/images/logos/dream_house.png" alt="">
@@ -334,10 +203,8 @@
                             </div>
                         </div>
                     </div>
-                    <!-- End Widget -->
                 </div>
                 <div class="col-md-4 col-xs-12 masonry-item">
-                    <!-- Widget -->
                     <div class="widget widget-article widget-shadow">
                         <div class="widget-header cover overlay overlay-hover">
                             <img class="cover-image overlay-scale" src="/assets/images/logos/dream_house.png" alt="">
@@ -367,10 +234,8 @@
                             </div>
                         </div>
                     </div>
-                    <!-- End Widget -->
                 </div>
                 <div class="col-md-4 col-xs-12 masonry-item">
-                    <!-- Widget -->
                     <div class="widget widget-article widget-shadow">
                         <div class="widget-header cover overlay overlay-hover">
                             <img class="cover-image overlay-scale" src="/assets/images/logos/dream_house.png" alt="">
@@ -400,10 +265,8 @@
                             </div>
                         </div>
                     </div>
-                    <!-- End Widget -->
                 </div>
                 <div class="col-md-4 col-xs-12 masonry-item">
-                    <!-- Widget -->
                     <div class="widget widget-article widget-shadow">
                         <div class="widget-header cover overlay overlay-hover">
                             <img class="cover-image overlay-scale" src="/assets/images/logos/dream_house.png" alt="">
@@ -433,10 +296,8 @@
                             </div>
                         </div>
                     </div>
-                    <!-- End Widget -->
                 </div>
                 <div class="col-md-4 col-xs-12 masonry-item">
-                    <!-- Widget -->
                     <div class="widget widget-article widget-shadow">
                         <div class="widget-header cover overlay overlay-hover">
                             <img class="cover-image overlay-scale" src="/assets/images/logos/dream_house.png" alt="">
@@ -466,10 +327,7 @@
                             </div>
                         </div>
                     </div>
-                    <!-- End Widget -->
                 </div>
-
-
             </div>
         </div>
     </div>
@@ -487,14 +345,14 @@
     <script src="/vendor/asscrollable/jquery.asScrollable.all.js"></script>
     <script src="/vendor/ashoverscroll/jquery-asHoverScroll.js"></script>
     <script src="/vendor/waves/waves.js"></script>
-    <!-- Plugins -->
+
     <script src="/vendor/switchery/switchery.min.js"></script>
     <script src="/vendor/intro-js/intro.js"></script>
     <script src="/vendor/screenfull/screenfull.js"></script>
     <script src="/vendor/slidepanel/jquery-slidePanel.js"></script>
     <script src="/vendor/toastr/toastr.js"></script>
     <script src="/vendor/bootstrap-treeview/bootstrap-treeview.min.js"></script>
-    <!-- Scripts -->
+
     <script src="/js/core.js"></script>
     <script src="/assets/js/site.js"></script>
     <script src="/assets/js/sections/menu.js"></script>
@@ -511,7 +369,4 @@
     <script src="/js/components/bootstrap-treeview.js"></script>
     <script src="/assets/examples/js/advanced/treeview.js"></script>
 
-
-{{--<script src="/vendor/bootstrap-treeview/bootstrap-treeview.min.js"></script>
-<script src="/assets/examples/js/advanced/treeview.js"></script>--}}
 @endsection
