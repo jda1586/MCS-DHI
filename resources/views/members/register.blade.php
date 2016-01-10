70f557ea13c4ada346de0c14ea252eec2c27b82c
jose <jose_asdrubal@hotmail.com>
2016-01-10 16:04:03 -0600

    mascara a registro**

--- resources/views/members/register.blade.php
+++ resources/views/members/register.blade.php
@@ -184,33 +184,9 @@
 @endsection
 
 @section('script')
-    {{--<script src="/vendor/jquery/jquery.js"></script>
-    <script src="/vendor/bootstrap/bootstrap.js"></script>
-    <script src="/vendor/animsition/animsition.js"></script>
-    <script src="/vendor/asscroll/jquery-asScroll.js"></script>
-    <script src="/vendor/mousewheel/jquery.mousewheel.js"></script>
-    <script src="/vendor/asscrollable/jquery.asScrollable.all.js"></script>
-    <script src="/vendor/ashoverscroll/jquery-asHoverScroll.js"></script>
-    <script src="/vendor/waves/waves.js"></script>--}}
-    <!-- Plugins -->
-    {{--<script src="/vendor/switchery/switchery.min.js"></script>
-    <script src="/vendor/intro-js/intro.js"></script>
-    <script src="/vendor/screenfull/screenfull.js"></script>
-    <script src="/vendor/slidepanel/jquery-slidePanel.js"></script>--}}
+
     <script src="/vendor/formatter-js/jquery.formatter.js"></script>
     <!-- Scripts -->
-    <script src="/js/core.js"></script>
-    <script src="../../assets/js/site.js"></script>
-    <script src="../../assets/js/sections/menu.js"></script>
-    <script src="../../assets/js/sections/menubar.js"></script>
-    <script src="../../assets/js/sections/sidebar.js"></script>
-    <script src="/js/configs/config-colors.js"></script>
-    <script src="../../assets/js/configs/config-tour.js"></script>
-    <script src="/js/components/asscrollable.js"></script>
-    <script src="/js/components/animsition.js"></script>
-    <script src="/js/components/slidepanel.js"></script>
-    <script src="/js/components/switchery.js"></script>
-    <script src="/js/components/tabs.js"></script>
     <script src="/js/components/formatter-js.js"></script>
 
     {{--<script src="vendor/formvalidation/formValidation.min.js"></script>--}}
