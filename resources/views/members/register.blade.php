d812a47bb6b936d1536628d4a0ee2f7406994367
aarn-mcs <aarn@mexcorpsoluciones.com>
2016-01-10 01:21:18 -0600

    Changes in frontviews

--- resources/views/members/register.blade.php
+++ resources/views/members/register.blade.php
@@ -2,6 +2,61 @@
 @section('head_scripts')
     <link rel="stylesheet" href="/assets/examples/css/pages/register-v2.css">
     <link rel="stylesheet" href="/vendor/slidepanel/slidePanel.css">
+
+    <link rel="stylesheet" href="/vendor/animsition/animsition.css">
+    <link rel="stylesheet" href="/vendor/asscrollable/asScrollable.css">
+    <link rel="stylesheet" href="/vendor/switchery/switchery.css">
+    <link rel="stylesheet" href="/vendor/intro-js/introjs.css">
+    <link rel="stylesheet" href="/vendor/slidepanel/slidePanel.css">
+    <link rel="stylesheet" href="/vendor/flag-icon-css/flag-icon.css">
+    <link rel="stylesheet" href="/vendor/waves/waves.css">
+    <link rel="stylesheet" href="/vendor/select2/select2.css">
+    <link rel="stylesheet" href="/vendor/bootstrap-tokenfield/bootstrap-tokenfield.css">
+    <link rel="stylesheet" href="/vendor/bootstrap-tagsinput/bootstrap-tagsinput.css">
+    <link rel="stylesheet" href="/vendor/bootstrap-select/bootstrap-select.css">
+    <link rel="stylesheet" href="/vendor/icheck/icheck.css">
+    <link rel="stylesheet" href="/vendor/switchery/switchery.css">
+    <link rel="stylesheet" href="/vendor/asrange/asRange.css">
+    <link rel="stylesheet" href="/vendor/asspinner/asSpinner.css">
+    <link rel="stylesheet" href="/vendor/clockpicker/clockpicker.css">
+    <link rel="stylesheet" href="/vendor/ascolorpicker/asColorPicker.css">
+    <link rel="stylesheet" href="/vendor/bootstrap-touchspin/bootstrap-touchspin.css">
+    <link rel="stylesheet" href="/vendor/card/card.css">
+    <link rel="stylesheet" href="/vendor/jquery-labelauty/jquery-labelauty.css">
+    <link rel="stylesheet" href="/vendor/bootstrap-datepicker/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="/vendor/bootstrap-maxlength/bootstrap-maxlength.css">
+    <link rel="stylesheet" href="/vendor/jt-timepicker/jquery-timepicker.css">
+    <link rel="stylesheet" href="/vendor/jquery-strength/jquery-strength.css">
+    <link rel="stylesheet" href="/vendor/multi-select/multi-select.css">
+    <link rel="stylesheet" href="/vendor/typeahead-js/typeahead.css">
+
+    <script src="/vendor/jquery/jquery.js"></script>
+    <script src="/vendor/animsition/animsition.js"></script>
+    <script src="/vendor/asscroll/jquery-asScroll.js"></script>
+    <script src="/vendor/mousewheel/jquery.mousewheel.js"></script>
+    <script src="/vendor/asscrollable/jquery.asScrollable.all.js"></script>
+    <script src="/vendor/ashoverscroll/jquery-asHoverScroll.js"></script>
+    <script src="/vendor/waves/waves.js"></script>
+    <!-- Plugins -->
+    <script src="/vendor/switchery/switchery.min.js"></script>
+    <script src="/vendor/intro-js/intro.js"></script>
+    <script src="/vendor/screenfull/screenfull.js"></script>
+    <script src="/vendor/slidepanel/jquery-slidePanel.js"></script>
+    <script src="/vendor/jquery-placeholder/jquery.placeholder.js"></script>
+    <!-- Scripts -->
+    <script src="/js/core.js"></script>
+    <script src="/assets/js/site.js"></script>
+    <script src="/assets/js/sections/menu.js"></script>
+    <script src="/assets/js/sections/menubar.js"></script>
+    <script src="/assets/js/sections/sidebar.js"></script>
+    <script src="/js/configs/config-colors.js"></script>
+    <script src="/js/configs/config-tour.js"></script>
+    <script src="/js/components/asscrollable.js"></script>
+    <script src="/js/components/animsition.js"></script>
+    <script src="/js/components/slidepanel.js"></script>
+    <script src="/js/components/switchery.js"></script>
+    <script src="/js/components/tabs.js"></script>
+    <script src="/js/components/jquery-placeholder.js"></script>
 @endsection
 
 @section('content')
@@ -57,7 +112,7 @@
                     <div class="form-group">
                         <div class="form-group">
                             <select class="form-control" name="country">
-                                <option value="">Pais</option>
+                                <option value="">Country</option>
                                 <option value="Estados Unidos">Estados Unidos</option>
                                 <option value="México">México</option>
                                 <option value="Canada">Canada</option>
@@ -71,12 +126,18 @@
                         </div>
                     </div>
                     <div class="form-group">
+                        <label class="sr-only" for="inputBirthday">Birthday</label>
+                        <input type="text" class="form-control" id="inputBirthday" name="birthday"
+                               placeholder="Birthday" data-plugin="datepicker">
+                    </div>
+                    <div class="form-group">
                         <label class="sr-only" for="inputEmail">Email</label>
                         <input type="email" class="form-control" id="inputEmail" name="email" placeholder="Email">
                     </div>
                     <div class="form-group">
                         <label class="sr-only" for="inputEmail">Email Confirm</label>
-                        <input type="email" class="form-control" id="inputEmailConfirm" name="email_confirm" placeholder="Email Confirm">
+                        <input type="email" class="form-control" id="inputEmailConfirm" name="email_confirm"
+                               placeholder="Email Confirm">
                     </div>
                     <div class="form-group">
                         <label class="sr-only" for="inputPassword">Password</label>
@@ -125,4 +186,6 @@
     {!! HTML::script('vendor/formvalidation/formValidation.min.js') !!}
     {!! HTML::script('vendor/formvalidation/framework/bootstrap.min.js') !!}
     {!! HTML::script('validator.js') !!}
+
+
 @endsection
