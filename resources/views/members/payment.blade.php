c936fa119076134d0316f2d4cbee39717c8db42b
aarn-mcs <aarn@mexcorpsoluciones.com>
2016-01-11 22:31:40 -0600

    payment view fix

--- resources/views/members/payment.blade.php
+++ resources/views/members/payment.blade.php
@@ -8,19 +8,20 @@
 @endsection
 
 @section('content')
+<div class="page-content">
     {{--<div>
-        <div id="wrapper-main" class="col-md-10 col-md-offset-1">
-            <div id="producto" class="col-md-5 col-md-offset-0" style="float: left">
-                <img src="/assets/images/logos/SILVER_PACK.png" alt="">
-                <div id="precio"> $______.00</div>
-            </div>
-            <div id="msj" class="col-md-7 col-md-offset-0" style="float: right">
-                <span>
-                    estas a paso, paga el paquete de < name > y comienza disfrutar de los beneficios
-                </span>
-            </div>
+    <div id="wrapper-main" class="col-md-10 col-md-offset-1">
+        <div id="producto" class="col-md-5 col-md-offset-0" style="float: left">
+            <img src="/assets/images/logos/SILVER_PACK.png" alt="">
+            <div id="precio"> $______.00</div>
         </div>
-    </div>--}}
+        <div id="msj" class="col-md-7 col-md-offset-0" style="float: right">
+            <span>
+                estas a paso, paga el paquete de < name > y comienza disfrutar de los beneficios
+            </span>
+        </div>
+    </div>
+</div>--}}
 
     <div class="page animsition" style="animation-duration: 800ms; opacity: 1;">
         <ol class="breadcrumb">
@@ -38,7 +39,11 @@
                     <div id="wrapper-main" class="col-md-10 col-md-offset-0">
                         <div id="producto" class="col-md-5 col-md-offset-0" style="float: left">
                             <img src="/assets/images/logos/{!! $new_user->product->image['url'] !!}" alt="">
-                            <div id="precio" class="" style="margin:15px;padding-left:40px; "> ${!! $new_user->product->price !!}</div>
+                            <div id="precio" class="" style="margin:15px;padding-left:40px; ">
+                                <div id="precio" class="text-left" style="margin:15px;padding:20px 0 0 0;">
+                                    <span style="font-weight: bold; color: black ">$ {!! $new_user->product->price !!}</span>
+                                </div>
+                            </div>
                         </div>
                         <div id="msj" class="col-md-7 col-md-offset-0" style="float: right; height: 50%">
                             <div style="margin: auto; height: 100px">
@@ -68,6 +73,7 @@
             </div>
         </div>
     </div>
+</div>
 @endsection
 
 @section('script')
