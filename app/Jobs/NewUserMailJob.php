832b215c11c72faf968136e7d25e0f2acce1d628
Pedro Luna <neko.hitokori@gmail.com>
2016-01-12 02:07:27 -0600

    correccion a NewUserMailJob

--- app/Jobs/NewUserMailJob.php
+++ app/Jobs/NewUserMailJob.php
@@ -4,19 +4,23 @@ namespace DHI\Jobs;
 
 use Mail;
 use DHI\Jobs\Job;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Bus\SelfHandling;
+use Illuminate\Contracts\Queue\ShouldQueue;
 
-class NewUserMailJob extends Job implements SelfHandling
+class NewUserMailJob extends Job implements SelfHandling, ShouldQueue
 {
+    use InteractsWithQueue, SerializesModels;
     /**                                                                                                            O
      * Create a new job instance.
      *
      */
-    protected $data=array();
+    protected $data = array();
 
     public function __construct($data)
     {
-         $this->data=$data;
+        $this->data = $data;
     }
 
     /**
@@ -26,13 +30,13 @@ class NewUserMailJob extends Job implements SelfHandling
      */
     public function handle()
     {
-        $this->data=$this->data[0];
+        $this->data = $this->data[0];
 //        $user=$this->data['user'];
-        $email=$this->data['email'];
-        $name=$this->data['name'].' '.$this->data['lastname'];
+        $email = $this->data['email'];
+        $name = $this->data['name'] . ' ' . $this->data['lastname'];
 
         dd($this->data);
-        Mail::send('emails.welcome',['data' => $this->data] , function ($message) use($email,$name)   {
+        Mail::send('emails.welcome', ['data' => $this->data], function ($message) use ($email, $name) {
             $message->from('servers@dreamhouseinternational.com', 'Dream House International');
             $message->to($email, $name)->subject('Confirmacion de registro');
         });
