cf9d45eafc4bb2817a1565ab09c8af54b84a25cc
Pedro Luna <neko.hitokori@gmail.com>
2016-01-26 21:00:45 -0600

    fix admin.members.index

--- resources/views/admin/members/index.blade.php
+++ resources/views/admin/members/index.blade.php
@@ -148,8 +148,7 @@
                             <tr data-url="/partial/{!! $user->user !!}" data-toggle="slidePanel" class="animation-fade"
                                 style="animation-fill-mode: backwards; animation-duration: 250ms; animation-delay: 100ms;">
                                 <td>
-                                    <img class="img-responsive"
-                                         src="/assets/images/logos/{{ ($user->product_id == 1) ? 'BRONZE_PACK.png' : ($user->product_id == 2) ? 'SILVER_PACK.png' : 'GOLD_PACK.png'}}" alt="...">
+                                    <img class="img-responsive" src="/assets/images/logos/{!! $user->product->image['url'] !!}">
                                 </td>
                                 <td class="cell-300">
                                     {{ $user->user }}
