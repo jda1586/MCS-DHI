5dd06de4402a241b31dc0a59646cb706bc548866
jose <jose_asdrubal@hotmail.com>
2016-02-03 01:21:41 -0600

    se modifico la consulta que muestra las utilidades

--- app/Http/Controllers/WalletsController.php
+++ app/Http/Controllers/WalletsController.php
@@ -1,15 +1,11 @@
 <?php
-
 namespace DHI\Http\Controllers;
-
 use DHI\BitcoinAccount;
 use Illuminate\Http\Request;
-
 use DHI\Http\Requests;
 use DHI\Http\Controllers\Controller;
 use Input;
 use Validator;
-
 class WalletsController extends Controller
 {
     /**
@@ -26,25 +22,22 @@ class WalletsController extends Controller
             'activation_movements' => auth()->user()->movements()
                 ->where('from', 'activation')->orWhere('to', 'activation')->orderBy('created_at', 'DESC')->take(20)->get(),
             'utilities_movements' => auth()->user()->movements()
-                ->where('from', 'utilities')->orWhere('to', 'utilities')->orderBy('created_at', 'DESC')->take(20)->get(),
+                ->where('user_id',Auth()->user()->getAuthIdentifier())->where('from', 'utilities')->orwhere('user_id',Auth()->user()->getAuthIdentifier())->Where('to', 'utilities')->orderBy('created_at', 'DESC')->take(20)->get(),
             'auction_movements' => auth()->user()->movements()
                 ->where('from', 'auction')->orWhere('to', 'auction')->orderBy('created_at', 'DESC')->take(20)->get(),
             'bitcoin_accounts' => auth()->user()->bitcoin_accounts,
         ]);
     }
-
     public function store()
     {
         $validator = Validator::make(Input::all(), [
             'nameaccount' => 'required|min:6',
             'numberaccount'=> 'required|min:27|max:34',
         ]);
-
         if ($validator->passes()) {
 //            $wallet = UserDeposit::where('user_id','=',Auth()->user()->getAuthIdentifier())->first();
             $nameAccount=Input::get('nameaccount');
             $numberAccount= Input::get('numberaccount');
-
             if($wallet = BitcoinAccount::create(array(
                 'user_id' => Auth()->user()->getAuthIdentifier(),
                 'name'=>$nameAccount,
@@ -52,7 +45,7 @@ class WalletsController extends Controller
                 'status'=>'pending',
                 'balance_in'=>0,
                 'balance_out'=>0,
-                ))
+            ))
             ){      //si se guardo la informacion ahoa si muevo el archivo
                 return redirect()->route('wallets.index');
             }else{
