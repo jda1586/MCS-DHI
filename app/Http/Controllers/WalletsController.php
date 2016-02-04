dee710c6fc6bf40ec60c581f26bb1428a29849a7
Pedro Luna <neko.hitokori@gmail.com>
2016-02-03 21:16:26 -0600

    correccion de WalletsController > index

--- app/Http/Controllers/WalletsController.php
+++ app/Http/Controllers/WalletsController.php
@@ -20,14 +20,18 @@ class WalletsController extends Controller
         $user = auth()->user();
         return view('wallets.index', [
             'wallets' => auth()->user()->wallets,
-            'commission_movements' => $user->movements()
-                ->where('from', 'commission')->orWhere('to', 'commission')->orderBy('created_at', 'DESC')->take(20)->get(),
-            'activation_movements' => $user->movements()
-                ->where('from', 'activation')->orWhere('to', 'activation')->orderBy('created_at', 'DESC')->take(20)->get(),
-            'utilities_movements' => $user->movements()
-                ->where('from', 'utilities')->orWhere('to', 'utilities')->orderBy('created_at', 'DESC')->take(20)->get(),
-            'auction_movements' => $user->movements()
-                ->where('from', 'auction')->orWhere('to', 'auction')->orderBy('created_at', 'DESC')->take(20)->get(),
+            'commission_movements' => $user->movements()->where(function ($q) {
+                $q->where('from', 'commission')->orWhere('to', 'commission');
+            })->orderBy('created_at', 'DESC')->take(20)->get(),
+            'activation_movements' => $user->movements()->where(function ($q) {
+                $q->where('from', 'activation')->orWhere('to', 'activation');
+            })->orderBy('created_at', 'DESC')->take(20)->get(),
+            'utilities_movements' => $user->movements()->where(function ($q) {
+                $q->where('from', 'utilities')->orWhere('to', 'utilities');
+            })->orderBy('created_at', 'DESC')->take(20)->get(),
+            'auction_movements' => $user->movements()->where(function ($q) {
+                $q->where('from', 'auction')->orWhere('to', 'auction');
+            })->orderBy('created_at', 'DESC')->take(20)->get(),
             'bitcoin_accounts' => $user->bitcoin_accounts,
         ]);
     }
