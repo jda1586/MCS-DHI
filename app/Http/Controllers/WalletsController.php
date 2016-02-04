1cef1e9a98f9d16213f9176bcb2084fd2ffe8cdf
Pedro Luna <neko.hitokori@gmail.com>
2016-02-03 21:08:13 -0600

    correccion de WalletsController > index

--- app/Http/Controllers/WalletsController.php
+++ app/Http/Controllers/WalletsController.php
@@ -17,17 +17,18 @@ class WalletsController extends Controller
      */
     public function index()
     {
+        $user = auth()->user();
         return view('wallets.index', [
             'wallets' => auth()->user()->wallets,
-            'commission_movements' => auth()->user()->movements
+            'commission_movements' => $user->movements()
                 ->where('from', 'commission')->orWhere('to', 'commission')->orderBy('created_at', 'DESC')->take(20)->get(),
-            'activation_movements' => auth()->user()->movements
+            'activation_movements' => $user->movements()
                 ->where('from', 'activation')->orWhere('to', 'activation')->orderBy('created_at', 'DESC')->take(20)->get(),
-            'utilities_movements' => auth()->user()->movements
+            'utilities_movements' => $user->movements()
                 ->where('from', 'utilities')->orWhere('to', 'utilities')->orderBy('created_at', 'DESC')->take(20)->get(),
-            'auction_movements' => auth()->user()->movements
+            'auction_movements' => $user->movements()
                 ->where('from', 'auction')->orWhere('to', 'auction')->orderBy('created_at', 'DESC')->take(20)->get(),
-            'bitcoin_accounts' => auth()->user()->bitcoin_accounts,
+            'bitcoin_accounts' => $user->bitcoin_accounts,
         ]);
     }
 
