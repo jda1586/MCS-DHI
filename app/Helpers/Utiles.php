642bfb9ac840d3ba10dcb2614e8fdbc29a7d3e64
miriam <miriam.rodriguez@corb.mx>
2016-02-04 23:51:20 -0600

    saving images items

--- app/Helpers/Utiles.php
+++ app/Helpers/Utiles.php
@@ -7,28 +7,28 @@ class utiles{
 
         $current_year  = date('Y');
         $current_month = date('m');
-        $file_path     = FILES.'/' . $current_year . '/' . $current_month;
-        if( !file_exists( FILES ) ){
-            mkdir( FILES, 0777, true );
+        $file_path     = 'Items'.'/' . $current_year . '/' . $current_month;
+        if( !file_exists( 'Items' ) ){
+            mkdir( 'Items', 0777, true );
         }
 
         if ( !( file_exists ( $file_path ) ) ) {
-            if ( file_exists ( FILES.'/' . $current_year ) ) {
+            if ( file_exists ( 'Items'.'/' . $current_year ) ) {
                 //Exist uploads/year
                 mkdir( $file_path, 0777, true);
                 chmod( $file_path, 0777);
             }
-            else if ( file_exists ( FILES.'/' ) ){
+            else if ( file_exists ( 'Items'.'/' ) ){
                 //At least exist the uploads folder
                 mkdir( $file_path, 0777, true);
-                chmod( FILES.'/' . $current_year, 0777);
+                chmod( 'Items'.'/' . $current_year, 0777);
                 chmod( $file_path, 0777);
             }
             else {
                 //Ok that is weird... well we create the uploads folder too
                 mkdir( $file_path, 0777, true);
-                chmod( FILES.'/', 0777);
-                chmod( FILES.'/' . $current_year, 0777);
+                chmod( 'Items'.'/', 0777);
+                chmod( 'Items'.'/' . $current_year, 0777);
                 chmod( $file_path, 0777);
             }
         }
